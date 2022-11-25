823c0d1541d4422893c14c582ef939021951e879
rynddev <manhnvph25559@fpt.edu.vn>
2022-11-25 15:35:08 +0700

    function updated

--- dao/room_dao.php
+++ dao/room_dao.php
@@ -2,7 +2,7 @@
 
 function rooms_list(){
     $query = "SELECT rooms.*, GROUP_CONCAT(storage_room.image) AS image FROM rooms INNER JOIN storage_room ON rooms.id = storage_room.id_room GROUP BY storage_room.id_room";
-    return pdo_query_all($query);
+    return pdo_query($query);
 }
 
 
