d75dcc20d97e0a1706a6952350419c5792d4e067
kens1ne <tanndph25572@fpt.edu.vn>
2022-12-09 18:00:18 +0700

    updated

--- site/footer.php
+++ site/footer.php
@@ -138,17 +138,11 @@
     crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script src="../public/js/style.js"></script>
 <script>
-<<<<<<< HEAD
 $(document).ready(function() {
     $('#table_id').DataTable();
 });
+AOS.init();
 </script>
-
-
-=======
-    AOS.init();
-</script>
->>>>>>> origin/main
 </body>
 
 </html>
